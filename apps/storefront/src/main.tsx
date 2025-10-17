da53a4617a9cf20df7fea198add02eb70bdb42f0
Phoenix-Falcon001 <bassameter005@gmail.com>
2025-10-17 11:20:27 +0300

    Fix: correct main.tsx render structure

--- apps/storefront/src/main.tsx
+++ apps/storefront/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d7b33812de4ec7f627231001a73b8fd0e86874b0e9d3a4c054ba17a2481b7af
-size 336
+oid sha256:b2b5a9446a2b6f147f0c863f3168ef2ecef4dbe971578d64517e9d9f85290d78
+size 342
